f33624866901cc635d73b0d632ade9f6529856bc
Ian Leifer <ian_leifer@MacBook-Air-Ian.local>
2017-09-28 13:33:15 -0400

    Added hw1 prob 1

---
+++ hw1.py
@@ -0,0 +1,13 @@
+import math
+
+def problem1():
+    print "Hello, user!\n"
+    g = float(9.8)
+    h = float(raw_input("Please input the height: "))
+    t = math.sqrt(2 * h / g)
+    print t
+    return 0
+
+def problem2():
+    
+    return 0
