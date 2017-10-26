ea16b9bf424d7fe0c357e82451c00330b710d05a
Ian Leifer <ian_leifer@MacBook-Air-Ian.fios-router.home>
2017-10-26 18:36:25 -0400

    Read configuration function implemented

--- courseWork/main.cpp
+++ courseWork/main.cpp
@@ -2,7 +2,6 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello world!\n");
     Table table;
     return 0;
 }
--- courseWork/table.cpp
+++ courseWork/table.cpp
@@ -1,8 +1,9 @@
 #include "table.h"
-#include <stdio.h>
+#include <iostream>
+#include <fstream>
 
 Table::Table() {
-    printf("Hello world from table!\n");
+    readConfig();
 }
 
 Table::~Table() {
@@ -12,6 +13,40 @@ void Table::tick() {
 }
 
 void Table::readConfig() {
+    std::string line;
+    std::ifstream config;
+    config.open ("config.txt", std::ifstream::in);
+    
+    // reading number of cells
+    std::getline(config, line, '\t');
+    std::getline(config, line, '\t');
+    numberOfCells = std::stoi(line);
+    std::getline(config, line, '\n');
+    
+    // reading cell type
+    std::getline(config, line, '\t');
+    std::getline(config, line, '\t');
+    cellType = std::stoi(line);
+    std::getline(config, line, '\n');
+    
+    // reading modeling time
+    std::getline(config, line, '\t');
+    std::getline(config, line, '\t');
+    modelingTime = std::stoi(line);
+    std::getline(config, line, '\n');
+    
+    // reading modeling time
+    std::getline(config, line, '\t');
+    std::getline(config, line, '\t');
+    phaseSpaceDimensionality = std::stoi(line);
+    std::getline(config, line, '\n');
+    config.close();
+    
+    // Printing out configuration to make sure it's right
+    std::cout << "Number of cells = "               << numberOfCells            << std::endl;
+    std::cout << "Cell type = "                     << cellType                 << std::endl;
+    std::cout << "Modeling time = "                 << modelingTime             << std::endl;
+    std::cout << "Phase space dimensionality = "    << phaseSpaceDimensionality << std::endl;
 }
 
 void Table::setupAdjacency() {
