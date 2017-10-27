55bc94f82c0c007b60dd464a40d900f6442afaa2
Ian Leifer <ian_leifer@MacBook-Air-Ian.fios-router.home>
2017-10-26 20:29:01 -0400

    Compiler improvement

--- courseWork/compile.sh
+++ courseWork/compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo -en "\033[42m Running simulation compilation process \033[0m"
 g++ main.cpp table.cpp cell.cpp variable.cpp -o simulator
 chmod 777 simulator
+echo -en "\033[41m Code successfully compiled. Running simulation \033[0m"
 ./simulator
