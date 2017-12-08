981d2e979a14acef0f66d773fbc83dbd6bce0fdc
Ian Leifer <ianleifer93@gmail.com>
2017-12-08 16:16:28 -0500

    Removed some useless comments

--- courseWork/run.py
+++ courseWork/run.py
@@ -23,16 +23,8 @@ config = configFile.readline()
 phaseSpaceDimensionality = int(re.search(r'\d+', config).group(0))
 configFile.close()
 
-# TODO: this needs rework, not safe at all
-#x = config.split("\n")
-#for i in range(len(x)):
-#    print(x[i])
-#    m = re.search(r'\d+', x[i])
-#    print(m.group(0))
-
 # reading output, now we know from where to read
 data = np.empty([numberOfCells, phaseSpaceDimensionality, modelingTime], dtype = float)
-#[[[0 for x in range(0, modelingTime)] for y in range(0, phaseSpaceDimensionality)] for z in range(0, numberOfCells)]
 fileNamePrefix = 'output'
 fileNamePostfix = '.txt'
 for i in range(numberOfCells):
@@ -47,16 +39,10 @@ for i in range(numberOfCells):
             data[i][j][k] = x[k]
         dataFile.close()
         
-#print(data)
-
 x = np.linspace(1, modelingTime, modelingTime) #makes an evenly spaced array 50 values between 0 and 6
 for i in range(numberOfCells):
     for j in range(phaseSpaceDimensionality):
         ll = 'Cell = ' + str(i) + '; dim = ' + str(j)
         plt.plot(x, data[i, j, :], label = ll)
-#y1=np.sin(x)
-#y2=np.cos(x)
-#plt.plot(x, y1, 'g.', label='y1')
-#plt.plot(x, y2)
 plt.legend(loc='upper right')
 plt.show() #show the plot if using interactive mode (otherwise does nothing)
