ed08571521ac4e613c1014c9f2da8b3e82bbb19e
Ian Leifer <ianleifer93@gmail.com>
2017-12-08 08:57:54 -0500

    Small comment

--- courseWork/run.py
+++ courseWork/run.py
@@ -1,5 +1,6 @@
 import subprocess
 
+# TODO: this needs rework
 output = subprocess.check_output("g++ main.cpp table.cpp cell.cpp variable.cpp oscillator.cpp -o simulator -std=c++11", shell=True)
 print(output)
 output = subprocess.check_output("chmod 777 simulator", shell=True)
