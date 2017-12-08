77185a121c68e6a0038ef9b8770ae462d5a21e4f
Ian Leifer <ianleifer93@gmail.com>
2017-12-08 18:53:14 -0500

    First attempt to apply Runge-Kutta

--- courseWork/oscillator.cpp
+++ courseWork/oscillator.cpp
@@ -8,6 +8,8 @@ Oscillator::Oscillator(int pphaseSpaceDimensionality) {
     setPhaseSpaceDimensionality(pphaseSpaceDimensionality);
 }
 
+/* In this function we can see that everything is hardcoded. When I wrote this function I meant
+that oscillator will always have 2 dimensions and all methods are for 2D problems */
 Variable Oscillator::solveEquation() {
     Variable var(phaseSpaceDimensionality);
     double h = 0.01;
@@ -17,8 +19,18 @@ Variable Oscillator::solveEquation() {
 //    std::cout << this << std::endl;
 //    std::cout << "x = " << xk << "; y = " << yk << std::endl;
     double delta = getConnectionContribution();
+    
+// This is Runge-Kutta method
+
+//    Variable k1(phaseSpaceDimensionality), k2(phaseSpaceDimensionality), k3(phaseSpaceDimensionality), k4(phaseSpaceDimensionality);
+//    k1.setVariable(0, h * yk);
+     
+    
+// This is Euler method, we want to compare it to 4th order Runge-Kutta and Leapfrog
+    /*
     xkp = xk + h * yk;
-    ykp = yk + h * delta;
+    ykp = yk + h * delta;*/
+    
 //    std::cout << "xp = " << xkp << "; yp = " << ykp << std::endl;
     var.setVariable(0, ykp);
     var.setVariable(1, xkp);
